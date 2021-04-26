a0ab3ecfc2217c2fb5cbe77bd4213c3899cb2589
Scott Mounce <Scott.Mounce@pathforward.us>
2021-04-26 07:15:23 -0700

    removed an extra space in the userpoolid string

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: ' us-east-2_sMV8LC5UW', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-2_sMV8LC5UW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'aqqm6hbmb743l7dt4rvjqb14d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
